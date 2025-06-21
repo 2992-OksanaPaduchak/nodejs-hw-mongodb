544da037ef4a4f16d67f2dc8bd3ed6ea2b3f8643
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-06-22 00:32:03 +0300

    homework 3

---
+++ src/routers/contacts.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+  postContactController,
+  deleteContactController,
+  getContactsByIdController,
+  getContactsController,
+  patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+
+router.post('/contacts', ctrlWrapper(postContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+export default router;
