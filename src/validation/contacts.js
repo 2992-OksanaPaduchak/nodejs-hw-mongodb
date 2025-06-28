eab5f0704fadb4f7b6a1279ca514cdd3b41b9674
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-06-28 18:36:50 +0300

    error path validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
+  name: Joi.string().min(3).max(20).messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have at least {#limit} characters',
     'string.max': 'Name should have at most {#limit} characters',
@@ -11,7 +11,6 @@ export const createContactSchema = Joi.object({
     .min(6)
     .max(16)
     .pattern(/^\+?[0-9]{6,16}$/)
-    .required()
     .messages({
       'string.pattern.base': 'Phone number must be digits and may start with +',
       'any.required': 'Phone number is required',
@@ -21,11 +20,8 @@ export const createContactSchema = Joi.object({
     'string.email': 'Email must be valid',
   }),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid('personal', 'work', 'home')
-    .required()
-    .messages({
-      'any.only': 'Contact type must be one of: personal, business, other',
-      'any.required': 'Contact type is required',
-    }),
+  contactType: Joi.string().valid('personal', 'work', 'home').messages({
+    'any.only': 'Contact type must be one of: personal, business, other',
+    'any.required': 'Contact type is required',
+  }),
 });
